07dfb7fc20348a3a9b5a2f4d6151156aab270c81
Robert James <robjames@evidently.ca>
2020-08-07 10:46:52 -0700

    adding a mkdir for the osp OscarDocument folders

--- demotestuser_01/testscript.sh
+++ demotestuser_01/testscript.sh
@@ -2,10 +2,15 @@ docker-compose stop
 docker-compose up -d
 pwd
 docker-compose logs  -t
-date > /tmp/junkfile
-cp /tmp/junkfile  /home/jenkins/workspace/demotestuser_01/volumes/OscarDocument/oscar/document/
+date >> /tmp/junkfile
+fallocate -l $((5*1024*1024)) /tmp/junkfile
+sync
+mkdir -p /home/jenkins/workspace/demotestuser_01/volumes/OscarDocument/oscar/document
+
+cp /tmp/junkfile /home/jenkins/workspace/demotestuser_01/volumes/OscarDocument/oscar/document/
+sync
 docker-compose logs  -t
-mv /tmp/junkfile /home/jenkins/workspace/demotestuser_01/volumes/OscarDocument/oscar/document/teleplanremitjunk
-sleep 20
+sudo rm -f /home/jenkins/workspace/demotestuser_01/volumes/OscarDocument/oscar/document/teleplanremitjunk
+rsync /tmp/junkfile /home/jenkins/workspace/demotestuser_01/volumes/OscarDocument/oscar/document/teleplanremitjunk
 docker-compose stop
 

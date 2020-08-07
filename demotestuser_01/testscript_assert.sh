7058eae061ac2cd2e9c9b74da13b6d77037f60b0
Shaun Luttin <admin@shaunluttin.com>
2020-08-07 15:58:03 -0700

    Clean up test files

--- demotestuser_01/testscript_assert.sh
+++ demotestuser_01/testscript_assert.sh
@@ -4,25 +4,19 @@ files=$(sftp -i sftp_rsa -b ./sftp_batch.sh demotestuser_01@pickup.evidently.ca)
 
 file=$(echo "$files" | tail -n 1 | tr -d '[:space:]')
 
-echo "$file"
-
 sftp -i sftp_rsa "demotestuser_01@pickup.evidently.ca:writable/$file"
 
 destination=$(echo "$file" | cut -f 1 -d '.')
 mkdir "$destination"
 tar -xvf "$file" -C "$destination"
 
+# Assert 
 testFileExists() {
-  echo "Testing existence: $1"
   assertTrue "[ -f $1 ]"
 }
 
 # shellcheck disable=SC1091
 source ./../shunit2/shunit2
 
-# Assert 
-
-# 1. There is a *.tar file.
-
-# 2. The *.tar file contains the `evidently_env` file.
+# The *.tar file contains the `evidently_env` file.
 testFileExists "$destination/debug/evidently_env"

65649527a4b9ec04165adc3576b8e8524860b79d
Shaun Luttin <admin@shaunluttin.com>
2020-08-07 12:20:04 -0700

    Create billing before remit

--- demotestuser_01/testscript.sh
+++ demotestuser_01/testscript.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
 dirOscarRemitFiles="/home/jenkins/workspace/demotestuser_01/volumes/OscarDocument/oscar/document"
-fileSourceRemit="/tmp/junkfile"
+dirOscarBillingFiles="/home/jenkins/workspace/demotestuser_01/volumes/OscarDocument/oscar/billing/download"
+fileSourceJunk="/tmp/junkfile"
 fileTargetRemit="$dirOscarRemitFiles/teleplanremitjunk"
+fileTargetBilling="$dirOscarBillingFiles/H0001"
 
 # Print the current working directory.
 pwd
 
 # Clean-up previous test artifacts.
-sudo rm -f $fileSourceRemit
+sudo rm -f $fileSourceJunk
 sudo rm -f $fileTargetRemit
+sudo rm -f $fileTargetBilling
 
 # Rebuild the docker image in the **parent** directory.
-sudo docker image build -t evidentlyslocker/openosp_evidently_demo:latest ..
-sudo docker push evidentlyslocker/openosp_evidently_demo:latest
+# sudo docker image build -t evidentlyslocker/openosp_evidently_demo:latest ..
+# sudo docker push evidentlyslocker/openosp_evidently_demo:latest
 
 # Restart Docker and follow its logs.
 docker-compose stop
 docker-compose up --force-recreate --build -d
 
 # Make a 5 MB junk file; write the date to it.
-fallocate -l $((5*1024*1024)) $fileSourceRemit
-date >> $fileSourceRemit
+fallocate -l $((5*1024*1024)) $fileSourceJunk
+date >> $fileSourceJunk
 
 # Create the oscar documents directory.
 mkdir -p $dirOscarRemitFiles
+mkdir -p $dirOscarBillingFiles
 
 # Copy the remit file to the oscar document directory.
 # This triggers the watchdog.
-rsync $fileSourceRemit $fileTargetRemit
+rsync $fileSourceJunk $fileTargetBilling
+rsync $fileSourceJunk $fileTargetRemit
 
 # Inspect the docker logs.
 docker-compose logs -f
